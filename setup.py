06189eddb7a42490e33a911c54aaf923a73328a5
manikan2406 <manikandan.c@eminds.ai>
2024-11-28 21:46:51 +0530

    setup updated2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "ollama-cli-test-case-generator=cli_markdown.cli:main",
+            "ollama-cli-test-case-generator=cli-markdown.cli:main",
         ],
     },
     classifiers=[
