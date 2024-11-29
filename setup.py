de78e5108b3f3548c615f238e9c6814cfe8d796a
manikan2406 <manikandan.c@eminds.ai>
2024-11-29 11:26:50 +0530

    setup updated6 with python ver

--- setup.py
+++ setup.py
@@ -24,13 +24,16 @@ setup(
     ],
     },
 
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+   classifiers=[
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+],
+
     include_package_data=True,
     zip_safe=False,
 )
